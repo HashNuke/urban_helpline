06465da5af164a14c3a147a8e6edb14899c34925
Akash Manohar J <akash@akash.im>
2013-04-06 13:07:25 +0530

    Initial app with mongoid, emberjs and devise added as dependencie

---
+++ README.md
@@ -0,0 +1,4 @@
+# Urban Helpline
+
+This is an app for the [12th plan hackathon](http://data.gov.in/hackathon/).
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrbanHelpline::Application.config.secret_key_base = 'ec02b6c12ee597c6a1cd14512b09ccb905eec0798cd3ed12fb404b5deef93a869407f409bfbd77b7e19d94ded7210c8889b73a9aaf7dd4d5d6e14cd7aaeb1330'
